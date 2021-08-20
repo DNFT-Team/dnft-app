d0f27a52d9057e323e1716a4c4c6006b5f355725
gakkiloves <mulander2011@outlook.com>
2021-08-20 15:22:47 +0800

    refactor: update layout of bridge

--- src/pages/bridge/index.js
+++ src/pages/bridge/index.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'ui-neumorphism';
+import {
+  Grid, GridItem, Box,
+  Heading, Text,
+  Input, InputGroup, InputLeftElement, InputRightAddon
+} from '@chakra-ui/react';
 import styles from './index.less';
-import {TextField, Button } from 'ui-neumorphism';
-import bgWindow from '../../images/bridge/bg_window.png'
-import {curveArrow} from '../../utils/svg'
-import {Grid, GridItem, Box, Heading, Text} from '@chakra-ui/react'
+import bgWindow from '../../images/bridge/bg_window.png';
+import { curveArrow } from '../../utils/svg';
 
-const BridgeScreen = (props) =>
+/* Available network*/
+const NetOptions = [
+  {key: 'eth', name: 'ETH', icon: '', netId: '', },
+  {key: 'bsc', name: 'BSC', icon: '', netId: '', },
+  {key: 'heco', name: 'HECO', icon: '', netId: '', },
+  // {key: 'polka', name: 'POLKA', icon: '', netId: '', },
+]
+
+const BridgeScreen = (props) => {
+  //  global loading
+  const [loading, setLoading] = useState(false)
+  //  networks
+  const [frNet, setFrNet] = useState(0)
+  const [toNet, setToNet] = useState(1)
+  //  form - input
+  const [amount, setAmount] = useState('')
+  const [balance, setBalance] = useState(5)
+
   //  render Dom
-  (
+  return (
     <div className="d-fullWidth">
       <div className={styles.main}>
         <Grid
@@ -34,7 +55,7 @@ const BridgeScreen = (props) =>
                     <div className={styles.winArrow}>{curveArrow('1')}</div>
                   </div>
                 </GridItem>
-                <GridItem colSpan={1}> </GridItem>
+                <GridItem colSpan={1}>&nbsp;</GridItem>
                 <GridItem colSpan={2} >
                   <div className={styles.winDR}>
                     <img src={bgWindow} alt=""/>
@@ -48,20 +69,52 @@ const BridgeScreen = (props) =>
 
             /* Cross-Chain Form */
             <GridItem colSpan={[10, 10, 10, 5, 6]} p={['1.785rem', '1.785rem', '1.785rem', '2rem', '3.785rem']} bg="brand.900">
-              <Box bg="rgb(227 235 245)" height="100%" borderRadius='.8rem' p='1rem' boxSizing='border-box'>
+              <Box bg="white" height="100%" borderRadius='.8rem' p='1rem' boxSizing='border-box'>
                 <div>
-                  <TextField placeholder="amount" />
-                  <p>Available balance : 100,000,000 DNF</p>
+                  <InputGroup>
+                    <InputLeftElement color="brand.600">
+                      <div className={styles.tokenType}>DNF</div>
+                    </InputLeftElement>
+                    <Input
+                      is-full-width="true"
+                      focusBorderColor="brand.600"
+                      borderColor="brand.300"
+                      placeholder="Amount"
+                      autoFocus
+                      style={{textIndent: '1rem'}}
+                      _hover={{borderColor: 'inherit'}}
+                      value={amount}  onChange={(v) => {setAmount(v.target.value)}}
+                    />
+                    <InputRightAddon>{
+                      NetOptions[frNet].name
+                    }</InputRightAddon>
+                  </InputGroup>
+                  <Text size="sm" color="brand.100">Available balance : ${balance} DNF</Text>
+                </div>
+                <div style={{margin: '3rem 0'}}>
+                  Here put A switchIcon, position right
                 </div>
                 <div>
-                 Here put A switchIcon, position right
+                  <InputGroup>
+                    <InputLeftElement color="brand.600">
+                      <div className={styles.tokenType}>DNF</div>
+                    </InputLeftElement>
+                    <Input
+                      is-full-width="true"
+                      focusBorderColor="brand.600"
+                      borderColor="brand.300"
+                      placeholder="Received"
+                      isReadOnly
+                      style={{textIndent: '1rem'}}
+                      _hover={{borderColor: 'inherit'}}
+                      value={amount}
+                    />
+                    <InputRightAddon>BSC</InputRightAddon>
+                  </InputGroup>
+                  <Text size="sm" color="brand.100">By now,You will received 100% DNF</Text>
                 </div>
-                <div>
-                  <TextField placeholder="accept" />
-                  Here needs a Select
-                </div>
-                <div>
-                  <Button>Confirm</Button>
+                <div style={{margin: '1rem 0'}}>
+                  <Button onClick={() => {setAmount('')}}>Confirm</Button>
                   <Button>History</Button>
                 </div>
               </Box>
@@ -71,5 +124,6 @@ const BridgeScreen = (props) =>
       </div>
     </div>
   )
+}
 
 export default BridgeScreen;
