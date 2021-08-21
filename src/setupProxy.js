4996f271f33915efc053517afcf8ab5588fa0805
gakkiloves <mulander2011@outlook.com>
2021-08-21 23:01:34 +0800

    refactor: remove bridge api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,4 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware(
-      '/nerve',
-      {
-        target: 'https://public.nerve.network',
-        changeOrigin: true,
-        pathRewrite: {
-          '/nerve': ''
-        }
-      }
-    )
-  );
 };
