49cb5fb269120facf9cfd95db9718b23e483f3c0
gakkiloves <mulander2011@outlook.com>
2021-08-27 17:58:13 +0800

    style: add particle-bg

--- src/pages/igo/index.js
+++ src/pages/igo/index.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { css } from 'emotion';
 import { Button, Box, Grid, GridItem } from '@chakra-ui/react';
 import { toast } from 'react-toastify';
+import ParticlesBg from 'particles-bg';
 
 const IGOScreen = (props) => {
   let history = useHistory();
@@ -20,28 +21,64 @@ const IGOScreen = (props) => {
     }
   }
 
+  let config = {
+    num: [4, 7],
+    rps: 0.1,
+    radius: [5, 30],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-40, 40],
+    alpha: [0.4, 0],
+    scale: [.1, 0.4],
+    position: 'all',
+    color: ['random', '#ff0000'],
+    cross: 'dead',
+    // emitter: "follow",
+    random: 15
+  };
+
+  if (Math.random() > 0.85) {
+    config = Object.assign(config, {
+      onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.rect(
+          particle.p.x,
+          particle.p.y,
+          particle.radius * 2,
+          particle.radius * 2
+        );
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    });
+  }
+
   return (
     <div className={styleIgo}>
-      <h3>Game List</h3>
-      <p className="describe">
-        <strong>Gamify</strong>
-        to adapt (a task) so that it takes on the form of a game
-      </p>
-      <Grid className="cardList"  gap={10} height="max-content"  templateColumns="repeat(10, 1fr)">
-        {
-          gameList.map((g) => (
-            <GridItem colSpan={[10, 10, 10, 5, 5]}>
-              <Box className="cardBox" bg="brand.100" p=".8rem">
-                <div className="avatar">{g.avatarUrl}</div>
-                <h4>{g.title}</h4>
-                <p>{g.description}</p>
-                <Button colorScheme="custom" onClick={() => {handlePlay(g)}}>Play</Button>
-              </Box>
-            </GridItem>
-          ))
-        }
-      </Grid>
-      <p className="describe">We do not own your private keys and cannot access your funds without your confirmation.</p>
+      <div className="content">
+        <h3>Game List</h3>
+        <p className="describe">
+          <strong>Gamify</strong>
+          to adapt (a task) so that it takes on the form of a game
+        </p>
+        <Grid className="cardList"  gap={10} height="max-content"  templateColumns="repeat(10, 1fr)">
+          {
+            gameList.map((g) => (
+              <GridItem colSpan={[10, 10, 10, 5, 5]}>
+                <Box className="cardBox" bg="brand.100" p=".8rem">
+                  <div className="avatar">{g.avatarUrl}</div>
+                  <h4>{g.title}</h4>
+                  <p>{g.description}</p>
+                  <Button colorScheme="custom" onClick={() => {handlePlay(g)}}>Play</Button>
+                </Box>
+              </GridItem>
+            ))
+          }
+        </Grid>
+        <p className="describe">We do not own your private keys and cannot access your funds without your confirmation.</p>
+      </div>
+      <ParticlesBg type="custom" config={config} bg className="particlesBg"/>
     </div>
   )
 }
@@ -54,42 +91,48 @@ const styleIgo = css`
   text-align: center;
   background: #FFFFFF;
   padding: 6.42rem 0;
-  h3 {
-    color: rgba(35, 38, 47, 1);
-    font-family: DM Sans, sans-serif;
-    font-weight: bold;
-    font-size: 3.429rem;
-    line-height: 4rem;
-    letter-spacing: .2rem;
-  }
-  .describe {
-    width: 100%;
-    align-items: center;
-    text-align: center;
-    color: rgba(119, 126, 144, 1);
-    font-family: Poppins, sans-serif;
-    font-size: 1rem;
-    line-height: 1.714rem;
-    box-sizing: border-box;
-    strong{
+  position: relative;
+  z-index: 0;
+  .content{
+    background: transparent;
+    h3 {
       color: rgba(35, 38, 47, 1);
-      margin: 0 .4rem;
+      font-family: DM Sans, sans-serif;
+      font-weight: bold;
+      font-size: 3.429rem;
+      line-height: 4rem;
+      letter-spacing: .2rem;
     }
-  }
-  .cardList{
-    padding: 1.5rem 0;
-    box-sizing: border-box;
-    width: 80%;
-    margin: 5.357rem auto;
-    .cardBox{
-      border-radius: 1.429rem;
+    .describe {
       width: 100%;
-      .avatar{
-        border-radius: 1.429rem;
-        background: #2C386F;
-        width: 100%;
-        height: 10rem;
+      align-items: center;
+      text-align: center;
+      color: rgba(119, 126, 144, 1);
+      font-family: Poppins, sans-serif;
+      font-size: 1rem;
+      line-height: 1.714rem;
+      box-sizing: border-box;
+      strong{
+        color: rgba(35, 38, 47, 1);
+        margin: 0 .4rem;
       }
     }
+    .cardList{
+      padding: 1.5rem 0;
+      box-sizing: border-box;
+      width: 60%;
+      margin: 5.357rem auto;
+      background: transparent;
+      .cardBox{
+        border-radius: 1.429rem;
+        width: 100%;
+        .avatar{
+          border-radius: 1.429rem;
+          background: #2C386F;
+          width: 100%;
+          height: 10rem;
+        }
+      }
+    } 
   }
 `
