b862f3a184ff6708b9676fc3c1b1517f1aba9654
gakkiloves <mulander2011@outlook.com>
2021-09-06 11:32:00 +0800

    style: update sideBar

--- src/layout/sideBar/index.js
+++ src/layout/sideBar/index.js
@@ -2,10 +2,14 @@ import * as React from 'react';
 import { motion, useCycle } from 'framer-motion';
 import { css, cx } from 'emotion';
 import { Icon } from '@iconify/react';
-import {Divider, Flex, Box, Text, Avatar} from '@chakra-ui/react'
+import { Divider, Flex, Box, Text, Avatar } from '@chakra-ui/react'
+import { toast } from 'react-toastify';
+import copy from 'copy-to-clipboard';
 import { MENU_MAP } from 'routers/config';
 import { MenuToggle } from './MenuToggle';
 import Logo from 'images/home/dnftLogo.png';
+import Head from 'images/asset/Head.svg';
+
 
 const connectIconArray = [
   {
@@ -108,9 +112,15 @@ export const SideBar = ({address, location, skipTo}) => {
           variants={variantsLi}
         >
           <Flex color="white" alignItems="center">
-            <Avatar src={Logo} width="2rem" height="2rem"/>
+            <Avatar src={Head} width="2rem" height="2rem" border="3px solid #fff"/>
             <Box flex="1" width="100%" px=".6rem"  boxSizing="border-box">
-              <Text isTruncated>Alice</Text>
+              <Text isTruncated>
+                <Icon icon="mdi:content-copy" style={{marginRight: '.8rem', cursor: 'pointer'}} onClick={() => {
+                  copy(address);
+                  toast.success('The address is copied successfully!', { position: toast.POSITION.TOP_CENTER });
+                }}/>
+                Wallet Address
+              </Text>
               <Text isTruncated>{address || '--'}</Text>
             </Box>
           </Flex>
@@ -156,7 +166,7 @@ export const SideBar = ({address, location, skipTo}) => {
 
 const styleMenuActive = css`
   background: #0049c6;
-  border-radius: 15px;
+  border-radius: 1.07rem;
 `
 const styleNav = css`
   position: fixed;
@@ -164,7 +174,8 @@ const styleNav = css`
   right: 0;
   bottom: 0;
   height: 100vh;
-  width: 280px;
+  //width: 280px;
+  width: 20rem;
   background: #233071;
   z-index: 9998;
 `
@@ -174,29 +185,30 @@ const styleBrandTitle = css`
   display: inline-flex;
   align-items: center;
   justify-content: space-evenly;
-  margin: .3rem .8rem 2rem .8rem;
+  padding: .714rem 2.14rem .714rem 2rem;
   user-select: none;
+  height: 4.571rem;
 `
 const styleUserCard = css`
   margin-top: 2rem;
-  padding-left: 25px;
+  padding-left: 1.7857rem;
   width: 80%;
 `
 const styleMenuUl = css`
-  padding: 25px;
+  padding: 1.7857rem;
   width: 80%;
   min-height: 50vh;
   overflow: auto;
 `
 const styleMenuLi = css`
   list-style: none;
-  margin-bottom: 20px;
+  margin-bottom: 1.4286rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
   svg{
-    font-size: 32px;
+    font-size: 2.2857rem;
     color: #FFFFFF;
   }
 `
@@ -205,8 +217,8 @@ const styleMenuRow = css`
   display: flex;
   align-items: center;
   img{
-    width: 40px;
-    height: 40px;
+    width: 2.857rem;
+    height: 2.857rem;
     border-radius: 50%;
     display: inline-block;
     margin-right: 1.2rem;
@@ -215,11 +227,11 @@ const styleMenuRow = css`
 const styleContactUs = css`
   display: flex;
   flex-direction: row;
-  gap: 24px;
+  gap: 2.12rem;
   justify-content: center;
   .item{
     text-decoration: none;
-    font-size: 23px;
+    font-size: 1.84rem;
     color: white;
     &:hover{
       svg{
