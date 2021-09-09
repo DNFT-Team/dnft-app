ff02993c5062ff783eb9aaadd8a582827ec62bb7
gakkiloves <mulander2011@outlook.com>
2021-09-09 18:57:02 +0800

    style: update loading

--- src/index.js
+++ src/index.js
@@ -62,26 +62,26 @@ if (mobileVal) {
   window.onresize = function () {setRem()}
 }
 
-
-ReactDOM.render(
-  // Fix---Warning: findDOMNode is deprecated in StrictMode
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <ChakraProvider resetCSS={false} theme={theme}>
-          <App />
-        </ChakraProvider>
-        <ToastContainer pauseOnFocusLoss={false}/>
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
-  ,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setTimeout(() => {
+  ReactDOM.render(
+    // Fix---Warning: findDOMNode is deprecated in StrictMode
+    // <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ChakraProvider resetCSS={false} theme={theme}>
+            <App />
+          </ChakraProvider>
+          <ToastContainer pauseOnFocusLoss={false}/>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    // </React.StrictMode>
+    ,
+    document.getElementById('root')
+  );
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}, 3000)
