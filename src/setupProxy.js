07b4ab09d8946192ebb9430608a7cc9e56361d14
gakkiloves <mulander2011@outlook.com>
2021-09-25 15:40:20 +0800

    refactor: add proxy local

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,9 +4,61 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(
+      '/api',
+      {
+        target: 'http://192.210.186.210:8077/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        },
+        secure: false
+      },
+    )
+  );
+  app.use(
+    createProxyMiddleware(
+      '/faucet',
+      {
+        target: 'http://92.205.29.153:3699/faucet',
+        changeOrigin: true,
+        pathRewrite: {
+          '/ipfsApi': ''
+        },
+        secure: false
+      },
+    )
+  );
+  app.use(
+    createProxyMiddleware(
       '/ipfsApi',
       {
-        target: 'https://bkapi.dnft.world/ipfsApi',
+        target: 'http://92.205.29.78:4003/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '/ipfsApi': ''
+        },
+        secure: false
+      },
+    )
+  );
+  app.use(
+    createProxyMiddleware(
+      '/ipfsGet',
+      {
+        target: 'http://92.205.29.78:4002/ipfs',
+        changeOrigin: true,
+        pathRewrite: {
+          '/ipfsApi': ''
+        },
+        secure: false
+      },
+    )
+  );
+  app.use(
+    createProxyMiddleware(
+      '/bridgeApi',
+      {
+        target: 'http://92.205.29.78:8081/bridgeApi',
         changeOrigin: true,
         pathRewrite: {
           '/ipfsApi': ''
